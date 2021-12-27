644a7b6bace362e652c9974325fa06868d42c5f3
cdmichaelb <broetjem@gmail.com>
2021-12-26 21:55:20 -0800

    changes

--- camel3amazon.user.js
+++ camel3amazon.user.js
@@ -116,7 +116,7 @@ GM_xmlhttpRequest({
 		);
 		$("body").append(bootstrapScript);
 		$("head").prepend(
-			`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" type="text/css" />`
+			`<link rel="stylesheet" href="https://raw.githubusercontent.com/cdmichaelb/Amazon-Camel-Graph/main/css/c3a.css" type="text/css" />`
 		);
 		$("head").append(
 			`<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>`
